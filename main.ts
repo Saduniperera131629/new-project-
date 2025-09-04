86646a2f34ba27d4f2717693dfa3632426b71197
Saduniperera131629 <saduniperera131629@gmail.com>
2025-09-04 21:28:04 +0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+let reading = 0
+led.setBrightness(64)
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(1)
+    reading = pins.analogReadPin(AnalogPin.P0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    led.plotBarGraph(
+    reading,
+    1023
+    )
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(reading)
+    }
+    basic.pause(5000)
+})
